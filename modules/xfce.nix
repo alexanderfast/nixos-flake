195346c9b5254d970ceb3a3a495d477e88a3f7ef
Alexander Fast <git@alexanderfast.se>
2024-02-24 10:41:15 +0100

    Add xfce

---
+++ modules/xfce.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce.enable = true;
+    };
+    displayManager.defaultSession = "xfce";
+  };
+}
