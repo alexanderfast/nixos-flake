0b66162fb6084edb7f6bd9c597ab5e9c69047483
Alexander Fast <git@alexanderfast.se>
2024-03-11 10:13:06 +0100

    Disable touchpad when typing

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -5,6 +5,7 @@
     [ ./hardware.nix ../../configuration.nix ../../modules/home-xfce4-i3.nix ];
 
   services.xserver.xkb.options = "ctrl:swapcaps";
+  services.xserver.libinput.touchpad.disableWhileTyping = true;
   networking.hostName = "laptop";
   networking.networkmanager.wifi.powersave = false;
 
