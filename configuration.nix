b229ceea2f210972ab10e6a8349ddad4d67caebd
Alexander Fast <git@alexanderfast.se>
2024-08-29 21:04:53 +0200

    Disable password auth

--- configuration.nix
+++ configuration.nix
@@ -190,7 +190,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
